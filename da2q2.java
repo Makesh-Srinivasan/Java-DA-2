f91909d1b179396e06f734e8d11e53c9cad61f82
Makesh Srinivasan <himakesh2001@gmail.com>
2021-04-30 14:23:17 +0530

    first commit

---
+++ da2q2.java
@@ -0,0 +1,19 @@
+// ead the aadhar number and Mobile Number of a employee.
+// If the aadhar number does not contain exactly 12 characters or if the Mobile Number does not contain exactly 10 characters, throw an IllegalArgumentException.
+// If the Mobile Number contains any character other than a digit, raise a NumberFormatException.
+// If the aadhar number contains any character other than digits and alphabets, throw a NoSuchElementException.
+// If they are valid, print the message ‘valid’ else ‘invalid’. Write a java program for the above scenario with an appropiate exceptions.
+// class Employee{
+//     String phone;
+//     String aadhar;
+//     Employee(String phone, String aadhar){
+//         this.phone = phone;
+//         this.aadhar = aadhar;
+//     }
+// }
+import java.util.Scanner;
+class da2q2{
+    public static void main(String args[]){
+        
+    }
+}
